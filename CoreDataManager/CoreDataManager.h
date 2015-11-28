f3cf4d056c517f16f7de996f62a82f40507d9762
Your Name HereGabrielMassana <gabrielmassana@gmail.com>
2015-11-28 10:53:54 +0000

    added h header

---
+++ CoreDataManager/CoreDataManager.h
@@ -0,0 +1,15 @@
+//
+//  CoreDataManager.h
+//  CoreDataManager
+//
+//  Created by Gabriel Massana on 28/11/15.
+//  Copyright © 2015 Gabriel Massana. All rights reserved.
+//
+
+#ifndef CoreDataManager_CoreDataManager_h
+#define CoreDataManager_CoreDataManager_h
+
+    #import "CDMCoreDataManager.h"
+    #import "CDMTableViewFetchedResultsController.h"
+
+#endif
