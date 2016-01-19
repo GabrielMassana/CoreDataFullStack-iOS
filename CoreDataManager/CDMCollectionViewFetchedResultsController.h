72b148667ad9e6d5452b907d5218189aaf1b838c
Your Name HereGabrielMassana <gabrielmassana@gmail.com>
2016-01-19 20:59:05 +0000

    CDMCollectionViewFetchedResultsController

---
+++ CoreDataManager/CDMCollectionViewFetchedResultsController.h
@@ -0,0 +1,27 @@
+//
+//  CDMCollectionViewFetchedResultsController.h
+//  CoreDataManager
+//
+//  Created by Gabriel Massana on 17/1/16.
+//  Copyright © 2016 Gabriel Massana. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+#import <UIKit/UIKit.h>
+
+@protocol CDMCollectionViewFetchedResultsControllerDelegate <NSObject>
+
+- (void)didUpdateContent;
+
+@end
+
+@interface CDMCollectionViewFetchedResultsController : NSFetchedResultsController
+
+@property (atomic, weak) id<CDMCollectionViewFetchedResultsControllerDelegate> frcDelegate;
+
+@property (nonatomic, weak) UICollectionView *collectionView;
+
+@property (nonatomic , assign) BOOL shouldUpdateSections;
+
+@end
---
+++ CoreDataManager/CDMCollectionViewFetchedResultsController.m
@@ -0,0 +1,195 @@
+//
+//  CDMCollectionViewFetchedResultsController.m
+//  CoreDataManager
+//
+//  Created by Gabriel Massana on 17/1/16.
+//  Copyright © 2016 Gabriel Massana. All rights reserved.
+//
+
+#import "CDMCollectionViewFetchedResultsController.h"
+
+@interface CDMCollectionViewFetchedResultsController () <NSFetchedResultsControllerDelegate>
+
+@property (nonatomic, strong) NSMutableArray *itemChanges;
+@property (nonatomic, strong) NSMutableArray *sectionChanges;
+
+@end
+
+@implementation CDMCollectionViewFetchedResultsController
+
+#pragma mark - Init
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        self.delegate = self;
+        
+        self.shouldUpdateSections = YES;
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithFetchRequest:(NSFetchRequest *)fetchRequest
+                managedObjectContext:(NSManagedObjectContext *)context
+                  sectionNameKeyPath:(NSString *)sectionNameKeyPath
+                           cacheName:(NSString *)name
+{
+    self = [super initWithFetchRequest:fetchRequest
+                  managedObjectContext:context
+                    sectionNameKeyPath:sectionNameKeyPath
+                             cacheName:name];
+    
+    if (self)
+    {
+        self.delegate = self;
+        
+        self.shouldUpdateSections = YES;
+    }
+    
+    return self;
+}
+
+#pragma mark - NSFetchedResultsControllerDelegate
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller
+{
+    self.sectionChanges = [[NSMutableArray alloc] init];
+    self.itemChanges = [[NSMutableArray alloc] init];
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller
+  didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex
+     forChangeType:(NSFetchedResultsChangeType)type
+{
+    if (self.shouldUpdateSections)
+    {
+        NSMutableDictionary *change = [[NSMutableDictionary alloc] init];
+        
+        change[@(type)] = @(sectionIndex);
+        
+        [self.sectionChanges addObject:change];
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath
+{
+    NSMutableDictionary *change = [[NSMutableDictionary alloc] init];
+    
+    switch(type)
+    {
+        case NSFetchedResultsChangeInsert:
+        {
+            change[@(type)] = newIndexPath;
+            
+            break;
+        }
+        case NSFetchedResultsChangeDelete:
+        {
+            change[@(type)] = indexPath;
+            
+            break;
+        }
+        case NSFetchedResultsChangeUpdate:
+        {
+            change[@(type)] = indexPath;
+            
+            break;
+        }
+        case NSFetchedResultsChangeMove:
+        {
+            change[@(type)] = @[indexPath, newIndexPath];
+            
+            break;
+        }
+    }
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
+{
+    [self.collectionView performBatchUpdates:^
+     {
+         for (NSDictionary *change in self.sectionChanges)
+         {
+             [change enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop)
+              {
+                  NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+                  
+                  switch(type)
+                  {
+                      case NSFetchedResultsChangeInsert:
+                      {
+                          [self.collectionView insertSections:[NSIndexSet indexSetWithIndex:[obj unsignedIntegerValue]]];
+                          
+                          break;
+                      }
+                      case NSFetchedResultsChangeDelete:
+                      {
+                          [self.collectionView deleteSections:[NSIndexSet indexSetWithIndex:[obj unsignedIntegerValue]]];
+                          
+                          break;
+                      }
+                      case NSFetchedResultsChangeUpdate:
+                      case NSFetchedResultsChangeMove:
+                      {
+                          break;
+                      }
+                  }
+              }];
+         }
+         for (NSDictionary *change in self.itemChanges)
+         {
+             [change enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop)
+              {
+                  NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+                  
+                  switch(type)
+                  {
+                      case NSFetchedResultsChangeInsert:
+                      {
+                          [self.collectionView insertItemsAtIndexPaths:@[obj]];
+                          
+                          break;
+                      }
+                      case NSFetchedResultsChangeDelete:
+                      {
+                          [self.collectionView deleteItemsAtIndexPaths:@[obj]];
+                          
+                          break;
+                      }
+                      case NSFetchedResultsChangeUpdate:
+                      {
+                          [self.collectionView reloadItemsAtIndexPaths:@[obj]];
+                          
+                          break;
+                      }
+                      case NSFetchedResultsChangeMove:
+                      {
+                          [self.collectionView moveItemAtIndexPath:obj[0]
+                                                       toIndexPath:obj[1]];
+                          
+                          break;
+                      }
+                  }
+              }];
+        }
+    }
+                                  completion:^(BOOL finished)
+    {
+        self.itemChanges = nil;
+        self.sectionChanges = nil;
+        
+        [self.frcDelegate didUpdateContent];
+    }];
+}
+
+@end
