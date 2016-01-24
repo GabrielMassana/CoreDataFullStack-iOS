ec26caf79993eecf7002b043bc70814e58ea554a
Your Name HereGabrielMassana <gabrielmassana@gmail.com>
2016-01-24 10:46:12 +0000

    Services

---
+++ CoreDataFullStack-iOS/Services/Count/CDFCountService.h
@@ -0,0 +1,13 @@
+//
+//  CDFCountService.h
+//  CoreDataFullStack
+//
+//  Created by Gabriel Massana on 24/1/16.
+//  Copyright © 2016 Gabriel Massana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDFCountService : NSObject
+
+@end
---
+++ CoreDataFullStack-iOS/Services/Deletion/CDFDeletionService.m
@@ -0,0 +1,13 @@
+//
+//  CDFDeletionService.m
+//  CoreDataFullStack
+//
+//  Created by Gabriel Massana on 24/1/16.
+//  Copyright © 2016 Gabriel Massana. All rights reserved.
+//
+
+#import "CDFDeletionService.h"
+
+@implementation CDFDeletionService
+
+@end
