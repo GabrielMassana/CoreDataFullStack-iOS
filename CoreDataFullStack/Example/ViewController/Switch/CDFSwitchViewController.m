5eb0bec13db8c2f1a460ca762938f1b59ad606c5
GabrielMassana <gabrielmassana@gmail.com>
2016-01-26 12:01:34 +0000

    switchVC buttons

--- CoreDataFullStack/Example/ViewController/Switch/CDFSwitchViewController.m
+++ CoreDataFullStack/Example/ViewController/Switch/CDFSwitchViewController.m
@@ -13,6 +13,10 @@
 
 @interface CDFSwitchViewController ()
 
+@property (nonatomic, strong) UIButton *easyTestButton;
+@property (nonatomic, strong) UIButton *tableViewButton;
+@property (nonatomic, strong) UIButton *collectionButton;
+
 @end
 
 @implementation CDFSwitchViewController
@@ -22,9 +26,77 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self.view addSubview:self.easyTestButton];
+    [self.view addSubview:self.tableViewButton];
+    [self.view addSubview:self.collectionButton];
+    
+    /*----------------*/
+
+    [self updateViewConstraints];
 }
 
 #pragma mark - Subviews
+
+- (UIButton *)easyTestButton
+{
+    if (!_easyTestButton)
+    {
+        _easyTestButton = [UIButton newAutoLayoutView];
+        
+        [_easyTestButton bbc_backgroundColorNormal:[UIColor lightGrayColor]
+                           backgroundColorSelected:[UIColor darkGrayColor]];
+        
+        [_easyTestButton setTitle:@"Easy Test Example"
+                         forState:UIControlStateNormal];
+        
+        [_easyTestButton addTarget:self
+                            action:@selector(easyTestButtonPressed:)
+                  forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    return _easyTestButton;
+}
+
+- (UIButton *)tableViewButton
+{
+    if (!_tableViewButton)
+    {
+        _tableViewButton = [UIButton newAutoLayoutView];
+        
+        [_tableViewButton bbc_backgroundColorNormal:[UIColor redColor]
+                           backgroundColorSelected:[UIColor purpleColor]];
+        
+        [_tableViewButton setTitle:@"Table View Example"
+                          forState:UIControlStateNormal];
+        
+        [_tableViewButton addTarget:self
+                             action:@selector(tableViewButtonPressed:)
+                   forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    return _tableViewButton;
+}
+
+- (UIButton *)collectionButton
+{
+    if (!_collectionButton)
+    {
+        _collectionButton = [UIButton newAutoLayoutView];
+        
+        [_collectionButton bbc_backgroundColorNormal:[UIColor orangeColor]
+                             backgroundColorSelected:[UIColor greenColor]];
+        
+        [_collectionButton setTitle:@"Collection View Example"
+                           forState:UIControlStateNormal];
+        
+        [_collectionButton addTarget:self
+                              action:@selector(collectionButtonPressed:)
+                   forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    return _collectionButton;
+}
 
 #pragma mark - Constraints
 
@@ -34,6 +106,58 @@
     
     /*----------------*/
     
+    [self.easyTestButton autoPinEdgeToSuperviewEdge:ALEdgeLeft];
+    
+    [self.easyTestButton autoPinEdgeToSuperviewEdge:ALEdgeRight];
+    
+    [self.easyTestButton autoSetDimension:ALDimensionHeight
+                                   toSize:70.0f];
+    
+    [self.easyTestButton autoPinEdgeToSuperviewEdge:ALEdgeTop
+                                          withInset:40.0f];
+    
+    /*----------------*/
+    
+    [self.tableViewButton autoPinEdgeToSuperviewEdge:ALEdgeLeft];
+    
+    [self.tableViewButton autoPinEdgeToSuperviewEdge:ALEdgeRight];
+    
+    [self.tableViewButton autoSetDimension:ALDimensionHeight
+                                   toSize:70.0f];
+    
+    [self.tableViewButton autoPinEdge:ALEdgeTop
+                               toEdge:ALEdgeBottom
+                               ofView:self.easyTestButton];
+    
+    /*----------------*/
+    
+    
+    [self.collectionButton autoPinEdgeToSuperviewEdge:ALEdgeLeft];
+    
+    [self.collectionButton autoPinEdgeToSuperviewEdge:ALEdgeRight];
+    
+    [self.collectionButton autoSetDimension:ALDimensionHeight
+                                     toSize:70.0f];
+    
+    [self.collectionButton autoPinEdge:ALEdgeTop
+                               toEdge:ALEdgeBottom
+                               ofView:self.tableViewButton];
+}
+
+#pragma mark - ButtonActions
+
+- (void)easyTestButtonPressed:(UIButton *)sender
+{
+    
+}
+
+- (void)tableViewButtonPressed:(UIButton *)sender
+{
+    
+}
+
+- (void)collectionButtonPressed:(UIButton *)sender
+{
     
 }
 
